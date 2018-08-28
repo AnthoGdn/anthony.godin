b16c17cc727f7b472554cbfd4827548c88a6febf
Anthony Godin <gdn.anthony@gmail.com>
2018-08-28 18:09:16 +0200

    create react app

---
+++ docs/precache-manifest.08191b53630712e81b4e1d95ce665522.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "57583d568c0535680d0c",
+    "url": "/anthony.godin/static/css/main.33836231.chunk.css"
+  },
+  {
+    "revision": "57583d568c0535680d0c",
+    "url": "/anthony.godin/static/js/main.57583d56.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/anthony.godin/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "bfdb8817052bda0f141b",
+    "url": "/anthony.godin/static/js/runtime~main.bfdb8817.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/anthony.godin/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d36991e4b19f197cdb8b11f627affed1",
+    "url": "/anthony.godin/index.html"
+  }
+];
